159ce7e7ecaf0308052c274b64d5530072f61017
Тимофей <tymcrit@gmail.com>
2020-03-06 16:34:53 +0300

    Добавлено чтение мночлена из командной строки

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,31 @@
 use num_complex::Complex;
 use rayon::prelude::*;
 use std::fs::File;
-use std::io::{stdin, stdout, BufWriter, Write};
+use std::io::BufWriter;
 use std::path::Path;
+use clap::{Arg, App};
 
-
-static polinom: [f64; 5] = [-1.0, 0.0, 2.0, 0.0, 1.0];
-
-fn f(x: Complex<f64>) -> Complex<f64> {
+fn f(x: Complex<f64>, polinom: &[f64]) -> Complex<f64> {
     polinom.iter().enumerate().map(|(i, k)| k * x.powi(i as i32)).sum()
 }
 
-fn g(x: Complex<f64>) -> Complex<f64> {
+fn g(x: Complex<f64>, polinom: &[f64]) -> Complex<f64> {
     polinom.iter().enumerate().skip(0).map(|(i, k)| k * i as f64 * x.powi(i as i32 - 1)).sum()
 }
 
-fn newton_func(n: Complex<f64>,  d: u8) -> (u8, u8, u8) {
-   if d == 31 || f(n).norm() < 1e-10 {
+fn newton_func(n: Complex<f64>, polinom: &[f64], d: u8) -> (u8, u8, u8) {
+   if d == 31 || f(n, polinom).norm() < 1e-10 {
         (255 - d * 8, 255 - d * 8, 255 - d * 8)
     } else {
-        newton_func(n - f(n) / g(n), d + 1)
+        newton_func(n - f(n, polinom) / g(n, polinom), polinom, d + 1)
     }
 }
 
-fn find_newton(x: Complex<f64>) -> (u8, u8, u8) {
-    newton_func(x, 0)
+fn find_newton(x: Complex<f64>, polinom: &[f64]) -> (u8, u8, u8) {
+    newton_func(x, polinom, 0)
 }
 
-fn newton((x1, y1): (f64, f64), (x2, y2): (f64, f64), h: u32) -> (u32, u32, Vec<u8>) {
+fn newton((x1, y1): (f64, f64), (x2, y2): (f64, f64), polinom: &[f64], h: u32) -> (u32, u32, Vec<u8>) {
     let w = ((x2 - x1) / (y2 - y1) * h as f64) as u32;
 
     (
@@ -40,7 +38,7 @@ fn newton((x1, y1): (f64, f64), (x2, y2): (f64, f64), h: u32) -> (u32, u32, Vec<
                     .map(|j| {
                         let re: f64 = x1 + ((x2 - x1) * j as f64 / w as f64);
                         let im: f64 = y1 + (y2 - y1) * i as f64 / h as f64;
-                        let (r, g, b) = find_newton(Complex { re, im });
+                        let (r, g, b) = find_newton(Complex { re, im }, polinom);
                         vec![
                             r,
                             g,
@@ -72,32 +70,60 @@ fn write_png(path: &str, (w, h): (u32, u32), data: &[u8]) -> Result<(), std::io:
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let h: u32 = {
-        print!("Введите высоту изображения: ");
-        stdout().flush()?;
-        let mut t = String::new();
-        stdin().read_line(&mut t)?;
-        t.trim().parse().unwrap()
-    };
+    let matches = App::new("Фракталы Ньютона")
+        .version("0.1")
+        .author("timcryt <tymcrit@gmail.com>")
+        .about("Генерирует фракталы Ньютона")
+        .arg(Arg::with_name("height")
+            .short("h")
+            .value_name("HEIGHT")
+            .help("Устанавливает высоту результурющего изображения")
+            .required(true)
+            .takes_value(true)
+            .validator(|v| match v.trim().parse::<u32>() {
+                Ok(x) if x > 0 => Ok(()),
+                _ => Err(String::from("Высота должна быть целым положтельным числом"))
+            }))
+        .arg(Arg::with_name("output")
+            .short("o")
+            .value_name("OUTPUT")
+            .help("Уставливает файл изображения")
+            .required(true)
+            .takes_value(true))
+        .arg(Arg::with_name("polinom")
+            .short("p")
+            .value_name("POLINOM")
+            .help("Устанавливает многочлен, по которуму строится фрактал")
+            .required(true)
+            .takes_value(true)
+            .validator(|v| {
+                for n in v.split(" ") {
+                    if n != "" {
+                        if let Err(_) = n.parse::<f64>() {
+                            return Err(String::from("Многочлен должен состоять из чисел"))
+                        }
+                    }
+                }
+                Ok(())
+            }))
+        .get_matches();
+
+    let h = matches.value_of("height").unwrap().trim().parse().unwrap();
+    let path = matches.value_of("output").unwrap();
+    let mut polinom = matches.value_of("polinom").unwrap().split(" ").filter_map(|x| if x == "" {None} else {Some(x.trim().parse::<f64>().unwrap())}).collect::<Vec<_>>();
+    polinom.reverse();
 
     let t = std::time::SystemTime::now();
 
-    let (w, h, v) = newton((-1.0, -1.0), (1.0, 1.0), h);
+    let (w, h, v) = newton((-1.0, -1.0), (1.0, 1.0), &polinom, h);
 
-    println!("{:?}", t.elapsed());
+    println!("Изображение сгенерировано за {:?}", t.elapsed().unwrap());
 
-    let path = {
-        print!("Введите имя файла для сохранения: ");
-        stdout().flush()?;
-        let mut t = String::new();
-        std::io::stdin().read_line(&mut t)?;
-        t.trim().to_string()
-    };
 
     let t = std::time::SystemTime::now();
 
     write_png(&path, (w, h), &v)?;
-    println!("{:?}", t.elapsed());
+    println!("Изображение записано за {:?}", t.elapsed().unwrap());
 
     Ok(())
 }
